6ab1a17e6e9d0b1cb8423b288299005260cd95c7
Gashanas <gashanas@gmail.com>
2017-12-19 20:22:13 +0200

    created home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,31 @@ import { RouterModule } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
 import { ModalComponent } from './shared/modal/modal.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    BlogPageComponent,
-    BlogCreationPageComponent,
-    HomeComponent,
-    ArticleComponent,
-    ModalComponent,
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        BlogPageComponent,
+        BlogCreationPageComponent,
+        HomeComponent,
+        ArticleComponent,
+        ModalComponent,
+    ],
+    imports: [
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        MaterialModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
