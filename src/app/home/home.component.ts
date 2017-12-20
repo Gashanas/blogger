37f66d4d10455127799d8c64e4260008cd159e9c
Gashanas <gashanas@gmail.com>
2017-12-20 05:34:37 +0200

    fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { BlogService } from '../services/blog.service';
+import { Blog } from '../models/blog.model';
 
 @Component({
     selector: 'app-home',
@@ -9,20 +11,42 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
 
-    title: string
+    title: string;
+    error: any;
+    blog : Blog;
 
-    constructor(private router: Router) {
+    constructor(private router: Router, private blogService: BlogService) {
     }
 
     ngOnInit() {
     }
 
-    navigateToBlogCreationPage() {
-        this.router.navigate(['customize'])
-    }
+    // navigateToBlogCreationPage() {
+    //     this.router.navigate(['customize'])
+    // }
 
     openBlog(){
         console.log(this.title);
+    }
+
+    getBlog(){
+        console.log(this.title);
+        this.blogService.getBlogInfo(this.title).subscribe(
+            (blog) => {
+                this.blog = blog;
+                console.log(this.blog,blog);
+                if(this.blog.id){
+                    this.error = '';
+                    this.router.navigate(['blog/'+this.title])
+                }
+            },
+            error => {
+            }
+        );
+
+        setTimeout(() => {
+            this.error = "blog does not exist";
+        }, 2000);
     }
 
 }
